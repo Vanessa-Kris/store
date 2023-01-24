f335ad69ff5faebc3d2ee788cbb663afd3a622f0
Minion <nessakristo@gmail.com>
2023-01-24 13:00:43 +0100

    Basic layout

--- store/src/pages/Home.js
+++ store/src/pages/Home.js
@@ -1,5 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+      <> <Container className='banner'>
+        <img src="./tamara-unsplash.jpg" alt="Bag" style={{ width: '100%', height: '100%' }} />
+        <div class="centered">
+          <h1>Daily Deals</h1>
+          <button type="button" class="btn btn-outline-light">Shop Now</button>
+        </div>
+        </Container>
+
+      <Container className='mt-5 mb-5'>
+          <Row>
+            <Col><img src="./black bag.jpeg" alt="Bag" style={{ width: '100%', height: '100%' }} /> <div>Bag <br/> 888</div> </Col>
+            <Col><img src="./Pink Bag.png" alt="Bag" style={{ width: '100%', height: '100%' }} /> <div>Bag <br/> 888</div> </Col>
+            <Col><img src="./Square Bag.jpeg" alt="Bag" style={{ width: '100%', height: '100%' }} /> <div>Bag <br/> 888</div> </Col>
+            <Col><img src="./Red Bag.jpg" alt="Bag" style={{ width: '100%', height: '100%' }} /> <div>Bag <br/> 888</div> </Col>
+
+          </Row>
+        </Container></>
+    )
   };
   
   export default Home;
--- store/src/pages/Layout.js
+++ store/src/pages/Layout.js
@@ -1,20 +1,44 @@
 import { Outlet, Link } from "react-router-dom";
 
+
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
+      <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+      <a class="navbar-brand" href="#Home">Stunners</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
+          <li class="nav-item">
+            <Link to="/blogs">About Us</Link>
           </li>
-          <li>
+          <li class="nav-item">
             <Link to="/contact">Contact</Link>
           </li>
+          <li class="nav-item">
+            <Link to="/contact">FAQ</Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/contact">Store Policies</Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/contact">Testimonials</Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/contact">Shop</Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/contact">Book online</Link>
+          </li>
         </ul>
+        </div>
+        </div>
       </nav>
 
       <Outlet />
