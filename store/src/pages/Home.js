8a182a137b331d4dffb2e2feab9d93659e62678a
Minion <nessakristo@gmail.com>
2023-01-24 21:20:02 +0100

    contact page

--- store/src/pages/Home.js
+++ store/src/pages/Home.js
@@ -3,6 +3,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 const Home = () => {
     return (
       <> <Container className='banner'>
@@ -13,7 +16,7 @@ const Home = () => {
         </div>
         </Container>
 
-      <Container className='mt-5 mb-5'>
+      <Container className='  homediv'>
           <Row>
             <Col xs={12} md={3}><img src="./black bag.jpeg" alt="Bag" style={{ width: '100%', height: '100%' }} /> <div>Bag <br/> 888</div> </Col>
             <Col xs={12} md={3}><img src="./Pink Bag.png" alt="Bag" style={{ width: '100%', height: '100%' }} /> <div>Bag <br/> 888</div> </Col>
@@ -22,7 +25,7 @@ const Home = () => {
           </Row>
         </Container>
         
-        <Container className='mb-5'>
+        <Container className='mb-5 homediv'>
           <Row className='mt-5 '>
             <Col xs={12} md={6} className='aboutdiv text-center text-white d-flex align-items-center justify-content-center'>
               <div>
@@ -41,6 +44,39 @@ const Home = () => {
             <img src="./tamara-unsplash.jpg" alt="Bag" style={{ width: '100%', height: '100%' }} />
             </Col>
           </Row>
+        </Container>
+
+        <Container className='mb-5 homediv'>
+        <Row className='mt-5 '>
+            <Col xs={12} md={6} className=' text-center  d-flex align-items-center justify-content-center'>
+            <h1 className=' '>Get Connected</h1> <br/>
+            <p>Bamenda, Cameroon</p> <br/>
+            <p>Stunners@gmail.com</p> <br/>
+            <p>650 39 33 69</p>
+            </Col>
+            <Col>
+            <Form>
+            <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" />
+             </Form.Group>
+
+             <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicText">
+                <Form.Label>Message</Form.Label>
+               <Form.Control type="text" placeholder="Enter message" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                Submit
+                </Button>
+                 </Form>
+            </Col>
+            </Row>
         </Container>
         </>
     )
